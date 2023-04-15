c71d5bb6f57b71cf9e3e4a5d032d57dc92d5e57f
Anton Zakharov <leroli.edu@gmail.com>
2023-04-15 07:42:54 +0500

    Solved [MEDIUM] 567. Permutation in String

--- [MEDIUM] 567. Permutation in String.swift
+++ [MEDIUM] 567. Permutation in String.swift
@@ -1,12 +1,21 @@
 class Solution {
     func checkInclusion(_ s1: String, _ s2: String) -> Bool {
-        let sortedS1 = s1.sorted()
-        let s = s2[0..<sortedS1.count]
-        print(s)
+        let countS2 = s2.count
+        let countS1 = s1.count
+        let s1Sorted = s1.sorted() 
+        for i in 0..<countS2 {
+            if i + countS1 > countS2 {
+                return false
+            } else {
+                let startIndex = String.Index(utf16Offset: i, in: s2)
+                let endIndex = String.Index(utf16Offset: i + countS1, in: s2)
+                let mySubstring = s2[startIndex..<endIndex]
+
+                if mySubstring.sorted() == s1Sorted {
+                    return true
+                }
+            }
+        }
         return false 
     }
 }
-
-let solution = Solution()
-let result = solution.checkInclusion("ba", "eidboaoo")
-print(result)
