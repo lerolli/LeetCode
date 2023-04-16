f1ff44b611795dfa1963eb407c2e47f83b246643
Anton Zakharov <leroli.edu@gmail.com>
2023-04-16 08:00:40 +0500

    Solved [EASY] 1672. Richest Customer Wealth

---
+++ [EASY] 1672. Richest Customer Wealth.swift
@@ -0,0 +1,14 @@
+/* Для себя
+* Решил использовать reduce, потому что идеально тут подходит. 
+* После reduce применяю max() и получаю максимальное значение.
+* Задачка оказалась простой
+*/
+
+class Solution {
+    func maximumWealth(_ accounts: [[Int]]) -> Int {
+        return accounts.map { $0.reduce(0, +) }.max() ?? 0
+    }
+}
+
+var solution = Solution()
+print(solution.maximumWealth([]))
