925d6fbccf07f73264952b07a47d5425440d5ec9
Anton Zakharov <leroli.edu@gmail.com>
2023-04-20 07:46:49 +0500

    Solved [MEDIUM] 662. Maximum Width of Binary Tree

--- [MEDIUM] 662. Maximum Width of Binary Tree.swift
+++ [MEDIUM] 662. Maximum Width of Binary Tree.swift
@@ -1,7 +1,39 @@
+// https://leetcode.com/problems/maximum-width-of-binary-tree/
+// TODO: Прийти через месяу и попробовать еще раз прорешать
+
 class Solution {
     func widthOfBinaryTree(_ root: TreeNode?) -> Int {
+        guard let root = root else {return 0}
+        
+        var queue : [(TreeNode,Int)] = [(root,0)]
+        var retVal = Int.min
+
+        while !queue.isEmpty {            
+            let count = queue.count
+            guard let first = queue.first, 
+                  let last = queue.last 
+            else { return 0 }
+            retVal = max(retVal, last.1 - first.1 + 1 )
+            let idx = first.1
             
-    }
+            for _ in 0..<count {
+                let first = queue.removeFirst()
+                
+                let node = first.0
+                let index = first.1 - idx
+                
+                if let left = node.left {
+                    let leftChildIndex = 2*index
+                    queue.append((left,leftChildIndex))
+                }
+                if let right = node.right {
+                    let rightChildIndex = 2*index+1
+                    queue.append((right,rightChildIndex))
+                }
+            }
+        }
+        return retVal
+    }  
 }
 
 final class TreeNode {
