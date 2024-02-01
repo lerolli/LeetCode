b2a9d52a96b805b651dedf9731cacfb2a8878bcb
Anton Zakharov <anton@Antons-MacBook-Air.local>
2024-02-01 12:02:18 +0500

    Solved [EASY] 125. Valid Palindrome

--- [EASY] 125. Valid Palindrome.swift
+++ [EASY] 125. Valid Palindrome.swift
@@ -6,7 +6,32 @@ class Solution {
         )
         return reversedS == cleanS
     }
+
+    func isPalindrome2(_ s: String) -> Bool {
+        var i = 0
+        var j = s.count - 1
+
+        while i <= j {
+            let leftElement = s.subscript(i)
+            let rightElement = s.subscript(j)
+            guard leftElement.isLetter || rightElement.isNumber else {
+                i += 1
+                continue
+            }
+
+            guard leftElement.isLetter || rightElement.isNumber else {
+                j -= 1
+                continue
+            }
+            if leftElement.lowercased() != rightElement.lowercased() {
+                return false
+            }
+            i += 1
+            j -= 1
+        }
+        return true
+    }
 }
 
 let solution = Solution()
-print(solution.isPalindrome("A man, a plan, a canal: Panama"))
+print(solution.isPalindrome2("ab2a"))
