fdf5f1738f154aba900c43d24119524809025a06
Anton Zakharov <anton@Antons-MacBook-Air.local>
2024-02-05 14:05:16 +0500

    Solved [EASY] 88.Merge Sorted Array

--- [EASY] 88.Merge Sorted Array.swift
+++ [EASY] 88.Merge Sorted Array.swift
@@ -1,9 +1,38 @@
 class Solution {
-    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    func merge_slow(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
         var newNum1 = nums1.dropLast(nums1.count - m)
         var newNum2 = nums2.dropLast(nums2.count - n)
         
         nums1 = (newNum1 + newNum2).sorted(by: <)
+    }
+
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var result: [Int] = []
+        var i = 0
+        var j = 0
+        while i < m || j < n {
+            if i >= m {
+                result.append(nums2[j])
+                j += 1
+                continue
+            }
+
+            if j >= n {
+                result.append(nums1[i])
+                i += 1
+                continue
+            }
+
+
+            if nums1[i] < nums2[j] {
+                result.append(nums1[i])
+                i += 1
+            } else {
+                result.append(nums2[j])
+                j += 1
+            }
+        }
+        nums1 = result
     }
 }
 
