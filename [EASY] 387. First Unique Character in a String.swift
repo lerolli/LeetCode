33f5d1e95b31c13733d795761b31b34568b2da06
Anton Zakharov <anton@Antons-MacBook-Air.local>
2024-02-05 14:05:16 +0500

    Solved [EASY] 387. First Unique Character in a String

--- [EASY] 387. First Unique Character in a String.swift
+++ [EASY] 387. First Unique Character in a String.swift
@@ -1,15 +1,38 @@
 class Solution {
     func firstUniqChar(_ s: String) -> Int {
-        var arr = Array(repeating: 0, count: 26)
-        let a = UnicodeScalar("a").value
-        s.unicodeScalars.forEach{
-            arr[Int($0.value - a)] += 1
+        let array = Array(s)
+        var dublicates: Set<Character> = []
+        
+        for i in 0..<array.count {
+            for j in i+1..<array.count {
+                if array[j] == array[i] {
+                    dublicates.insert(array[i])
+                    break
+                }
+            }
+            if !dublicates.contains(array[i]) {
+                return i
+            }
         }
-        var iter = 0
-        for ch in s.unicodeScalars {
-            if arr[Int(ch.value - a)] == 1 { return iter }
-            iter += 1
+        return -1
+    }
+
+    func firstUniqChar2(_ s: String) -> Int {
+        var dublicates: [Character: Int] = [:]
+
+        for char in s {
+            dublicates[char] = (dublicates[char] ?? 0) + 1 
+        }
+        
+        var i = 0
+        for char in s {
+            if dublicates[char] == 1 {
+                return i
+            }
+            i += 1
         }
         return -1
     }
 }
+
+print(Solution().firstUniqChar2("loveleetcode"))
